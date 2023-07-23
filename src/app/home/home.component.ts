070abb0e40d5d30a40c3f23fbb2832599a80fe8d
shahrzad <shahrzadjkoushesh@gmail.com>
2023-07-23 11:46:56 +0330

    add search logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/food';
-import { FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
-  foods:Food[] = [];
+  foods: Food[] = [];
 
-  constructor(private foodService:FoodService){
+  constructor(private foodService: FoodService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
-    this.foods = this.foodService.getAllFoods();
-
+    this.route.params.subscribe(params => {
+      if (params['searchTerm'])
+        this.foods = this.foodService.getAllFoods().filter(food =>
+          food.name.toLowerCase().includes(params['searchTerm'].toLowerCase()));
+      else
+        this.foods = this.foodService.getAllFoods();
+    })
   }
 
 }
